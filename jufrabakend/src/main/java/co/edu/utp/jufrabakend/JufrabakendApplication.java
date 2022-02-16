ae22f11ab7c3a09c89550c1d7b1f8b2cab771d89
oscarmauriciogiraldo <oscardan@utp.edu.co>
2022-02-16 10:35:26 -0500

    inicio spring boot backend

---
+++ jufrabakend/src/main/java/co/edu/utp/jufrabakend/JufrabakendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.jufrabakend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JufrabakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JufrabakendApplication.class, args);
+	}
+
+}
---
+++ jufrabakend/src/test/java/co/edu/utp/jufrabakend/JufrabakendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.jufrabakend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JufrabakendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
